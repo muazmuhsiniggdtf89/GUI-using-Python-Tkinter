b2246cbcfe5c8e0ca89430e2620ce4edf30382a6
muazmuhsiniggdtf89 <mohibmodmr@gmail.com>
2025-10-10 20:17:30 +0600

    Create Number Pad.py

---
+++ Number Pad.py
@@ -0,0 +1,26 @@
+from tkinter import*
+
+root = Tk()
+root.title ('Number Pad')
+root.geometry('250x300')
+
+frame = Frame(master=root, height=200, width=300, bg="#d0efff")
+
+nums = [[9, 8, 7], [6, 5, 4], [3, 2, 1], ['#', 0, '*']]
+
+for i in range(4):
+
+    root.columnconfigure(i, weight=1, minsize = 75)
+    root.rowconfigure(i, weight=1, minsize=50)
+    for j in range(0, 3):
+        frame = Frame(
+            master=root,
+            relief=SUNKEN,
+            borderwidth=1
+        )
+        frame.grid(row=i, column=j)
+        label =Label(master=frame, text=nums[i][j],bg="#d0efff")
+
+        label.pack(padx=3, pady =3)
+
+root.mainloop()
